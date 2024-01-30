a4aa744815e17adca72e64dcfd0997c59a13e9d4
Adnan7209 <mdadnan28713@gmail.com>
2024-01-31 01:35:10 +0530

    basic structure of App.js and routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React from "react";
+import {Route, Routes} from 'react-router-dom';
+import { Box } from "@mui/material";
 
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
+
+
+
+
+import "./App.css"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto" >
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/exercise/:id" element={<ExerciseDetail/>} />
+      </Routes>
+      <Footer/>
+
+    </Box>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import React from "react";
+import ReactDOM  from "react-dom";
+
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  
 )
