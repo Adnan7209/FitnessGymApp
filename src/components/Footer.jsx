ad33d050aec3809514baecfdca43d800e230c2d6
Adnan7209 <mdadnan28713@gmail.com>
2024-02-01 03:00:11 +0530

    footer section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Box,Stack,Typography} from '@mui/material'
+import Logo from "../assets/images/Logo-1.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#ffff3f4"> 
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt='logo' width="200px" heigth="200px"/>
+        <Typography variant='h5' pb="40px" mt="30px">
+          Made by Mohammed Adnan
+        </Typography>
+
+      </Stack>
+
+    </Box>
   )
-}
+} 
 
 export default Footer
